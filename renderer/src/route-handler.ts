37932f92b83e8971fc40053d26505274c55a9103
Achraf B <a.boutat4@gmail.com>
2022-03-23 17:16:06 +0100

    Revert "[28] - fix cover photo"
    This reverts commit 1b46d061565ae05f5304fbf1da390ec5fe3afd1f.

--- renderer/src/route-handler.ts
+++ renderer/src/route-handler.ts
@@ -718,24 +718,8 @@ export class RouteHandler {
             let coverImageHtml = ``;
 
             if (profile.metadata?.coverImage) {
-                coverImageHtml += `
-            <img class="w-full h-auto text-center" src="${profile.metadata?.coverImage}" style="
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    left: 0;
-                    max-width: 512px;
-                    max-height: 288px;
-                    margin-left: auto;
-                    margin-right: auto;
-                    z-index: -1;
-            " alt="cover image">`;
-            } else {
                 // language=HTML
                 coverImageHtml += `
-                    <div id="theme-html">
-                        <div class="sl-banner"></div>
-                    </div>
                     <style>
                         img.nc-avatar {
                             border: solid 2px #FFF;
@@ -1385,6 +1369,10 @@ export class RouteHandler {
                             ${headlineHtml}
                             ${subtitleHtml}
                             ${linkHtml}
+                            <!-- Theme html -->
+                            <div id="theme-html">
+                                <div class="sl-banner"></div>
+                            </div>
                             <!-- Watermark -->
                             ${watermarkHtml}
                             ${coverImageHtml}
