0650c6fa4206de30a9cbac8c15c43cb154e19dd5
achraf289 <a.boutat4@gmail.com>
2022-09-26 21:01:20 +0100

    fix: fix button

--- editor/components/page-management/ActionsComponent.vue
+++ editor/components/page-management/ActionsComponent.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="buttons">
     <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded text-sm"
-        @click="selectProfile"
-    >
-      <font-awesome-icon icon="fa-solid fa-check"/>
-      Select
-    </button>
-    <button
         v-if="!data.status || data.status === 'free'"
         class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded text-sm"
         @click="initCheckout"
@@ -35,7 +28,6 @@
 </template>
 <script lang="ts">
 import Vue from 'vue';
-import EventBus from "~/plugins/eventbus";
 
 export default Vue.extend({
   name: 'ActionsComponent',
@@ -46,9 +38,6 @@ export default Vue.extend({
     }
   },
   methods: {
-    selectProfile() {
-      EventBus.$emit('selectProfile', {profileId: this.data.id});
-    },
     async initCheckout() {
       try {
         const token = this.$store.getters['auth/getToken'];
--- editor/components/page-management/ImageComponent.vue
+++ editor/components/page-management/ImageComponent.vue
@@ -1,12 +1,16 @@
 <template>
-  <img
-      :src="data.imageUrl ? data.imageUrl : 'https://www.gravatar.com/avatar/'"
-      alt="no-image"
-      class="rounded-full h-8 w-8"
->
+  <button>
+    <img
+        :src="data.imageUrl ? data.imageUrl : 'https://www.gravatar.com/avatar/'"
+        alt="no-image"
+        class="rounded-full h-8 w-8"
+        @click="selectProfile"
+    >
+  </button>
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import EventBus from "~/plugins/eventbus";
 
 export default Vue.extend({
   name: 'ImageComponent',
@@ -15,6 +19,11 @@ export default Vue.extend({
       type: Object,
       required: true
     }
+  },
+  methods: {
+    selectProfile() {
+      EventBus.$emit('selectProfile', {profileId: this.data.id});
+    },
   }
 });
 </script>
