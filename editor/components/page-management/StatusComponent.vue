cb096e8eb8dbabfb639f5b6a23bc2990904a3aa3
achraf289 <a.boutat4@gmail.com>
2022-10-21 17:28:27 +0100

    fix: capitalize visibility and status

--- editor/components/page-management/StatusComponent.vue
+++ editor/components/page-management/StatusComponent.vue
@@ -3,7 +3,8 @@
   <div
       :class="{'py-1 px-2 mb-1 rounded-full text-sm leading-tight': true, 'bg-green-200 text-green-500': data.status === 'pro'}"
       style="width: fit-content"
-  >{{ data.status ? data.status : 'free' }}</div>
+  >{{ data.status ? data.status.replace(/^./, str => str.toUpperCase()) : 'Free' }}
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue';
